7df164f50f4d2b77c53fc76f7ea00f920748b729
Gabriel Gabriel <gabriel.gaby1277@gmail.com>
2026-08-24 18:44:42 +0700

    Fix topup-status route and enable realtime live auto polling

--- routes/web.php
+++ routes/web.php
@@ -64,10 +64,9 @@ Route::get('/produk/{slug}', [\App\Http\Controllers\PublicProductController::cla
 Route::get('/cv', [\App\Http\Controllers\CvController::class, 'index'])->name('cv.index');
 Route::get('/cv/create', [\App\Http\Controllers\CvController::class, 'create'])->name('cv.create');
 Route::get('/cv/preview-example/{slug}', [\App\Http\Controllers\CvController::class, 'previewExample'])->name('cv.previewExample');
-// Topup Game Routes (Publik & Guest Friendly)
+Route::get('/topup-status/{slug}', [\App\Http\Controllers\TopUpController::class, 'stockStatus'])->name('topup.stock-status');
 Route::get('/topup', [\App\Http\Controllers\TopUpController::class, 'index'])->name('topup.index');
 Route::get('/topup/{slug}', [\App\Http\Controllers\TopUpController::class, 'show'])->name('topup.show');
-Route::get('/topup/{slug}/stock-status', [\App\Http\Controllers\TopUpController::class, 'stockStatus'])->name('topup.stock-status');
 Route::post('/topup/{slug}/check-nickname', [\App\Http\Controllers\TopUpController::class, 'checkNickname'])->name('topup.check-nickname');
 Route::post('/topup/{slug}/process', [\App\Http\Controllers\TopUpController::class, 'process'])->name('topup.process');
 Route::get('/topup/checkout/{id}', [\App\Http\Controllers\TopUpPaymentController::class, 'show'])->name('topup.checkout.show');
