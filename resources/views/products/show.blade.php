976b4ba9678c63e8a5eb77e93e161dfb2309200a
Gabriel Gabriel <gabriel.gaby1277@gmail.com>
2026-08-26 21:18:10 +0700

    Update product plans feature list from Support email to Bantuan CS WhatsApp Cepat

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -92,7 +92,7 @@
                                 </li>
                                 <li class="flex items-center">
                                     <svg class="w-5 h-5 text-green-500 mr-2 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>
-                                    Support email
+                                    Bantuan CS WhatsApp Cepat
                                 </li>
                             </ul>
 
