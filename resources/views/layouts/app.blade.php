4a95a77cacedeff8243adb7be9acdea78898abe2
Gabriel Gabriel <gabriel.gaby1277@gmail.com>
2026-09-21 20:25:36 +0700

    feat: pasang Google site verification meta tag untuk Google Search Console

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="oxfKQl2rW_hSKMa0neoDOXt1oeVRlOwxuADP8NE4fkg" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="oxfKQl2rW_hSKMa0neoDOXt1oeVRlOwxuADP8NE4fkg" />
         <title>ToTap Store - Enterprise Software Solutions & Top Up Game</title>
 
         <!-- Early Theme Initialization to prevent flicker -->
